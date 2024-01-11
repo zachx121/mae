5656fc30dd69d4922033baec9ccaa2ba13211667
zhoutongzt <zhoutongzt@didiglobal.com>
2024-01-11 20:33:47 +0800

    use sort instead. (argsort not onnx-able)

--- run_mgeo.sh
+++ run_mgeo.sh
@@ -7,13 +7,13 @@ export PYTHONPATH=./src
 
 CUDA_VISIBLE_DEVICES=0
 python main_pretrain.py \
-    --save_ckpt_epoch 20 \
+    --save_ckpt_epoch 10 \
     --mask_ratio 0.5 \
-    --input_size 20 --patch_size 1 \
-    --embed_dim 1024 --depth 4 --num_heads 16\
-    --decoder_embed_dim 64 --decoder_depth 2 --decoder_num_heads 16\
-    --output_dir /nfs/volume-100001-6/zhoutongzt/MGeo/mgeo_torch_ps1enc4dec2 \
-    --log_dir /nfs/volume-100001-6/zhoutongzt/MGeo/mgeo_torch_ps1enc4dec2 \
+    --input_size 20 --patch_size 2 \
+    --embed_dim 768 --depth 12 --num_heads 12 \
+    --decoder_embed_dim 256 --decoder_depth 8 --decoder_num_heads 16 \
+    --output_dir /nfs/volume-100001-6/zhoutongzt/MGeo/mgeo_torch_ps2enc12dec8 \
+    --log_dir /nfs/volume-100001-6/zhoutongzt/MGeo/mgeo_torch_ps2enc12dec8 \
     --data_path /nfs/volume-100001-6/zhoutongzt/MGeo/feature_fm
 
 
